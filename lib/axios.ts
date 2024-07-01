4abe161fb55898dcd1d646a7d4df0e404827056a
PhatMenghor <phatmenghor@PhatMenghors-MacBook-Pro.local>
2024-07-01 08:15:15 +0700

    push

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // BaseURL for fetch Data from API
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.VERCEL_URL,
   timeout: 60000,
   headers: { "Content-Type": "application/json" },
 });
