2957a7d6d4b8bcbfa402224dca39e521c73226ab
PhatMenghor <phatmenghor@PhatMenghors-MacBook-Pro.local>
2024-07-01 08:25:01 +0700

    push

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // BaseURL for fetch Data from API
 const axiosInstance = axios.create({
-  baseURL: process.env.VERCEL_URL,
+  baseURL: "https://todo-menghor.vercel.app",
   timeout: 60000,
   headers: { "Content-Type": "application/json" },
 });
